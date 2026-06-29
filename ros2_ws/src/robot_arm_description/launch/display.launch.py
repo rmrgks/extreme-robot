7fa934e2cb055dd13a35eead4710f2e56449d2ae
ksp118 <78691366+ksp118@users.noreply.github.com>
2026-06-29 12:02:45 +0000

    feat: add MoveIt Dynamixel bridge

--- ros2_ws/src/robot_arm_description/launch/display.launch.py
+++ ros2_ws/src/robot_arm_description/launch/display.launch.py
@@ -19,8 +19,8 @@ def generate_launch_description():
             parameters=[{'robot_description': robot_description}],
         ),
         Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
         ),
         Node(
             package='rviz2',
